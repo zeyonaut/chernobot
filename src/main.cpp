670758c320c258459a97e5e9517fc6df69c8af41
aaronhyperum <unysnhyperum@gmail.com>
2018-03-14 19:38:21 +0800

    Qt dependency removed (temporarily) with a cleanup of main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,21 @@
 #include <imgui.h>
-#include <libserialport.h>
-#include <vector>
-#include <exception>
+#include <serial/serial.h>
 
 #include "nimir/graphics.h"
 #include "nimir/gui.h"
-#include <iostream>
-#include <chrono>
-#include <algorithm>
-using namespace std::chrono;
-
-/*extern "C" {
-#include <libavdevice/avdevice.h>
-#include <libswscale/swscale.h>
-}*/
-
-#include <stdio.h>
-
-#include <string>
-using std::string;
-using std::vector;
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#include <serial/serial.h>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <stdlib.h>
-
-#include <QApplication>
-#include <QGridLayout>
-#include <QGroupBox>
-#include <QComboBox>
-
-const double PI = 3.1415926;
+#include <vector>
+using std::vector;
+#include <exception>
+#include <iostream>
+#include <string>
+using std::string;
 
 struct vec2
 {
@@ -48,11 +27,10 @@ vec2 fright_motor = {-1, 1};
 vec2 bleft_motor = {-1, 1};
 vec2 bright_motor = {1, 1};
 
-int max = 0;
-
 // calculate power of motor - tx = x (100 to -100), ty = y(100 to -100), motor is motor's vector magnitudes
 int magnitude(const int tx, const int ty, const vec2 motor)
 {
+	const double PI = 3.1415926;
 	// where {tx, ty} is the target vector, {motor.x, motor.y} is the motor's (fixed) vector,
 
 	//if the target vector has no magnitude, the magnitude of the motor is none.
@@ -73,12 +51,6 @@ int magnitude(const int tx, const int ty, const vec2 motor)
 
 	float angle = atan2(ty, tx);
 	result *= cos(4 * (angle))/2 + 1.5;
-	//printf ("%0.0f\n", result);
-
-	//TODO: divide the result by the 100/160;
-	//
-	///if ((tx + ty) < 0) if (max < -(tx + ty)) max = -(tx + ty);
-	//else if (max < (tx + ty)) max = (tx + ty);
 
 	return -(int)result;
 }
@@ -90,19 +62,6 @@ char to_hex(int num)
 	if (num < 10) return (char) (48 + num);
 
 	else return (char) ((num - 10) + 65);
-}
-
-//convert 0d[-0d100, 0d100] -> 0x[0d0, 0d200]
-const char* conv(int num)
-{
-	num += 100;
-	if (num > 200) num = 200;
-	if (num < 0) num = 0;
-	int first = num/16;
-	int last = num%16;
-	std::string x(1, to_hex(first));
-	x.push_back(to_hex(last));
-	return x.c_str();
 }
 
 //convert 0d[0d0, 0d200] -> 0x[0d0, 0d200]
@@ -117,11 +76,17 @@ const char* conv_norm(int num)
 	return x.c_str();
 }
 
+//convert 0d[-0d100, 0d100] -> 0x[0d0, 0d200]
+const char* conv(int num)
+{
+	return conv_norm(num + 100);
+}
+
 
 int main(int, char**)
 {
 	GLFWwindow* window = NULL;
-	nimir::gl::init(window, 1280, 720, "example");
+	nimir::gl::init(window, 480, 720, "ARE WE BLIND?! DEPLOY THE GARRISON!");
 	nimir::gui::init(window, false);
 	serial::Serial port("");
 	port.setBaudrate(115200);
@@ -130,7 +95,6 @@ int main(int, char**)
 	glfwSetScrollCallback(window, nimir::gui::scrollCallback);
 	glfwSetKeyCallback(window, nimir::gui::keyCallback);
 	glfwSetCharCallback(window, nimir::gui::charCallback);
-	bool show_test_window = true;
 
 	float* axes = NULL;
 
@@ -142,44 +106,33 @@ int main(int, char**)
 	int clockwise = 0;
 
 	// Motor values, from -100 to 100;
-
 	int fleft = 0;
 	int fright = 0;
 	int bleft = 0;
 	int bright = 0;
-
 	int fvert = 0;
 	int bvert = 0;
 
 	bool opening = false;
 	bool closing = false;
 	bool pressing_camera = false;
-	// Selected Joystick in Combo Box
 
+	// Selected Joystick in Combo Box
 	int variableIndex = -1;
 
 	int portIndex = -1;
-
 	bool portOpen = false;
 
 	int is_lemming = 0;
 
-	// Main loop
-
-	// Loading images
-
 	serial::Serial prt("");
 	prt.setBaudrate(115200);
 
-	//Main loop
 	while (nimir::gl::nextFrame(window))
 	{
-
 		nimir::gui::nextFrame();
 		{
 			ImGui::Begin("Input");
-
-			//sample image: //ImGui::Image(&m_texture, ImVec2(128, 80), ImVec2(0,0), ImVec2(1,1), ImVec4(255,255,255,255), ImVec4(255,255,255,0));
 
 			ImGui::Text("Application running at %.1f FPS", ImGui::GetIO().Framerate);
 
@@ -201,7 +154,7 @@ int main(int, char**)
 
 				string output = "";
 
-				if (true)
+				if (1==1)
 				{
 					ImGui::Combo("Serial Port", &portIndex, ports, portinfo.size());
 
@@ -235,7 +188,7 @@ int main(int, char**)
 
 						ImGui::RadioButton("Saw", &is_lemming, 0); ImGui::SameLine();
 						ImGui::RadioButton("Lem", &is_lemming, 1); ImGui::SameLine();
-						ImGui::RadioButton("SORL", &is_lemming, 2);
+						ImGui::RadioButton("S+O+R+L", &is_lemming, 2);
 
 						ImGui::Text("Forward: %d", forward);
 						ImGui::Text("Right: %d", right);
@@ -243,22 +196,12 @@ int main(int, char**)
 						ImGui::Text("Clockwise: %d", clockwise);
 						ImGui::Text("Claw State: %s", opening? "Opening" : closing? "Closing" : "Neutral");
 
-						fleft = magnitude(right, forward, fleft_motor);
-						//fleft = (fleft + clockwise)/2;
-						fright = magnitude(right, forward, fright_motor);
-						//fright = (fright - clockwise)/2;
-						bleft = magnitude(right, forward, bleft_motor);
-						//bleft = (bleft + clockwise)/2;
-						bright = magnitude(right, forward, bright_motor);
-						//bright = (bright - clockwise)/2;
+						fleft = magnitude(right, forward, fleft_motor) + clockwise;
+						fright = magnitude(right, forward, fright_motor) - clockwise;
+						bleft = magnitude(right, forward, bleft_motor) + clockwise;
+						bright = magnitude(right, forward, bright_motor) - clockwise;
 						fvert = up;
 						bvert = up;
-
-						fleft += clockwise;
-						fright -= clockwise;
-						bleft  += clockwise;
-						bright -= clockwise;
-
 
 						ImGui::Text("Forward-Left Motor: %d", fleft);
 						ImGui::Text("Forward-Right Motor: %d", fright);
@@ -267,27 +210,9 @@ int main(int, char**)
 						ImGui::Text("Forward-Up Motor: %d", fvert);
 						ImGui::Text("Backward-Up Motor: %d", bvert);
 
-						ImGui::Text(pressing_camera? "Noob" : "Boon");
+						ImGui::Text(pressing_camera? "The camera is being pressed" : "The Chimera is being dragged into hyperspace\n by giant floating WHALES");
 
 						output = "T";
-/*
-						output.append(conv((int)(fright)));//2 4
-						output.append(conv((int)(-fleft)));//3
-						output.append(conv((int)(-bright)));//4 5
-						output.append(conv((int)(bleft)));//5
-
-						output.append(conv((int)fvert));//6 1
-						output.append(conv((int)bvert));//7 2
-
-						std::string camout = pressing_camera? conv_norm(0) :  conv_norm(180);
-
-						output.append(closing? conv_norm(0) : conv_norm(90));//8
-						output.append(closing? conv_norm(0) : conv_norm(90));//9
-						output.append(closing? conv_norm(0) : conv_norm(90));//10
-						output.append(closing? conv_norm(0) : conv_norm(90));//11
-						output.append(camout);
-						output.append(closing? conv_norm(0) : conv_norm(90));//12*/
-						// 5 claw, 2 forward motors and 2 vertical motors
 						if (is_lemming == 0)
 						{
 							output.append(conv((int)fvert));//6 1
@@ -349,18 +274,8 @@ int main(int, char**)
 						}
 
 						ImGui::Text("Output: %s", output.c_str());
-
-					   // std::cout << output << std::endl;
-
-
-
-						//Left up: Axis 1
-						//Left Horizontal: Axis 0
-						//Right Horizontal: Axis 2
-						//Right up: Axis 3
-						//Green: Button 1
-						//Red: Button 2
 					}
+
 					try
 					{
 						if (portIndex != -1)
@@ -395,12 +310,6 @@ int main(int, char**)
 			ImGui::End();
 		}
 
-
-		/*if (show_test_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
-			ImGui::ShowTestWindow(&show_test_window);
-		}*/
 		nimir::gl::setClearColor(ImColor(114, 144, 154));
 
 		nimir::gui::drawFrame();
