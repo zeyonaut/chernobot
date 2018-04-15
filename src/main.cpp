5e7a31c66693d29c3f2c44502f780132172d11da
aaronhyperum <unysnhyperum@gmail.com>
2018-04-15 10:11:48 +0800

    Started long-awaited refactor of dashboard/overseer.

--- src/main.cpp
+++ src/main.cpp
@@ -17,65 +17,7 @@ using std::vector;
 #include <array>
 using std::string;
 
-struct vec2
-{
-	float x, y;
-};
-// x is positive on the left, y is positive in front
-vec2 fleft_motor = {1, 1};
-vec2 fright_motor = {-1, 1};
-vec2 bleft_motor = {-1, 1};
-vec2 bright_motor = {1, 1};
-
-// calculate power of motor - tx = x (100 to -100), ty = y(100 to -100), motor is motor's vector magnitudes
-int magnitude (const int tx, const int ty, const vec2 motor)
-{
-	const double PI = 3.1415926;
-	// where {tx, ty} is the target vector, {motor.x, motor.y} is the motor's (fixed) vector,
-
-	//if the target vector has no magnitude, the magnitude of the motor is none.
-	if (((tx < 10 && tx >= 0)||(tx > -10 && tx <= 0)) && ((ty < 10 && ty >= 0 )||(ty > -10 && ty <= 0))) return 0;
-
-	//dot product of the target and motor vectors.
-	float dot_product = motor.x * tx + motor.y * ty;
-	//magnitudes
-	float target_mag = sqrt(tx * tx + ty * ty);
-	float motor_mag = sqrt(motor.x * motor.x + motor.y * motor.y);
-
-	//the cosine of the angle between the vectors is the dot product over the product of their magnitudes.
-	float costheta = dot_product / (target_mag * motor_mag);
-
-	float result = (acos(costheta) * 2) / PI; // acos -> [0 to PI] -> [0 to 2PI] -> [0 to 2]
-	result -= 1; //[-1 to 1]
-	result *= target_mag;
-
-	float angle = atan2(ty, tx);
-	result *= cos(4 * (angle))/2 + 1.5;
-
-	return -(int)result;
-}
-
-int curve (int input)
-{
-	double curve_magnitude = 2;
-	int max_value = 100;
-
-	if (input > 0)
-	{
-		return (int)(round(pow((input/(max_value/pow((double)(max_value),1/(curve_magnitude)))), curve_magnitude)));
-	}
-	else
-	{
-		return -(int)(round(pow((input/(max_value/pow((double)(max_value),1/(curve_magnitude)))), curve_magnitude)));
-	}
-}
-
-std::string serialize_data (std::array<unsigned char, 12> pin_data)
-{
-	std::string serialized_data = "ÿ";
-	for (unsigned char i : pin_data) serialized_data.push_back(i);
-	return serialized_data;
-}
+#include "overseer.h"
 
 int main (int, char**)
 {
@@ -125,30 +67,9 @@ int main (int, char**)
 
 	std::array<unsigned char, 12> pin_data;
 
-	float* axes = NULL;
-
-	// Control values, from -100 to 100
-
-	int forward = 0;
-	int right = 0;
-	int up = 0;
-	int clockwise = 0;
-
-	// Motor values, from -100 to 100;
-	int fleft = 0;
-	int fright = 0;
-	int bleft = 0;
-	int bright = 0;
-	int fvert = 0;
-	int bvert = 0;
-
-	int moclaw = 0;
-
-	bool opening = false;
-	bool closing = false;
-	bool pressing_camera = false;
-
 	// Selected Joystick in Combo Box
+	Controls c;
+
 	int variableIndex = -1;
 
 	int portIndex = -1;
@@ -230,79 +151,28 @@ int main (int, char**)
 
 			if (3 < axcount)
 			{
-				forward = -1 * (int) (100 * rawAxes[1]);
-				right = (int) (100 * rawAxes[0]);
-				up = -1 * (int) (100 *rawAxes[3]);
-				clockwise = (int) (100 *rawAxes[2]);
+				c.forward = -1 * (int) (100 * rawAxes[1]);
+				c.right = (int) (100 * rawAxes[0]);
+				c.up = -1 * (int) (100 *rawAxes[3]);
+				c.clockwise = (int) (100 *rawAxes[2]);
 			}
 
 			if (3 < bucount)
 			{
-				opening = rawButtons[1] == GLFW_PRESS;
-				closing = rawButtons[2] == GLFW_PRESS;
-				pressing_camera = rawButtons[3] == GLFW_PRESS;
-				if (opening == true && closing == true) opening = closing = false;
+				//opening = rawButtons[1] == GLFW_PRESS;
 			}
 
-			ImGui::Text("Forward: %d", forward);
-			ImGui::Text("Right: %d", right);
-			ImGui::Text("Up: %d", up);
-			ImGui::Text("Clockwise: %d", clockwise);
-			ImGui::Text("Claw State: %s", opening? "Opening" : closing? "Closing" : "Neutral");
 		}
 
-		ImGui::SliderInt("Forward", &forward, -100.f, 100.f);
-		ImGui::SliderInt("Right", &right, -100.f, 100.f);
-		ImGui::SliderInt("Up", &up, -100.f, 100.f);
-		ImGui::SliderInt("Clockwise", &clockwise, -100.f, 100.f);
+		ImGui::SliderInt("Forward", &c.forward, -100.f, 100.f);
+		ImGui::SliderInt("Right", &c.right, -100.f, 100.f);
+		ImGui::SliderInt("Up", &c.up, -100.f, 100.f);
+		ImGui::SliderInt("Clockwise", &c.clockwise, -100.f, 100.f);
 
-		ImGui::SliderInt("Chlaw", &moclaw, -100.f, 100.f);
+		ImGui::SliderInt("Chlaw", &c.moclaw, -100.f, 100.f);
 
-		fleft = magnitude(curve(right), curve(forward), fleft_motor) + curve(clockwise);
-		fright = magnitude(curve(right), curve(forward), fright_motor) - curve(clockwise);
-		bleft = magnitude(curve(right), curve(forward), bleft_motor) + curve(clockwise);
-		bright = magnitude(curve(right), curve(forward), bright_motor) - curve(clockwise);
-		fvert = curve(up);
-		bvert = curve(up);
+		serialize_controls(pin_data, c, is_lemming);
 
-		ImGui::Text("Forward-Left Motor: %d", fleft);
-		ImGui::Text("Forward-Right Motor: %d", fright);
-		ImGui::Text("Backward-Left Motor: %d", bleft);
-		ImGui::Text("Backward-Right Motor: %d", bright);
-		ImGui::Text("Forward-Up Motor: %d", fvert);
-		ImGui::Text("Backward-Up Motor: %d", bvert);
-		ImGui::Text(pressing_camera? "The Chimera is being pressed" : "The Chimera is being dragged into hyperspace\n by giant floating WHALES");
-
-		pin_data.fill(0 + 100);
-		if (is_lemming == 0) //Saw
-		{
-			pin_data[0] = -1 * bleft + 100;//6 1
-			pin_data[1] = bvert + 100;//7 2 TODO: FIGURE OUT WHETHER THE 'BACK ELEVATION' IS ON THE LEFT OR THE RIGHT. I believe the back elevation is actually right side.
-
-			pin_data[3] = -1 *(fright) + 100;//2 4
-			pin_data[4] = (bright) + 100;//4 5
-
-			pin_data[5] = (fvert) + 100;//5 6
-			pin_data[6] = (fleft) + 100;//3 7
-
-			pin_data[2] = moclaw + 100;
-		}
-		else if (is_lemming == 1) //Lem
-		{
-			pin_data[0] = (-forward - clockwise) + 100;//6 1
-			pin_data[1] = (-forward + clockwise) + 100;//7 2
-
-			pin_data[2] = (up) + 100;//2 4
-			pin_data[3] = (up) + 100;//4 5
-		}
-		else // ROMuLuS
-		{
-			pin_data[0] = (forward + clockwise) + 100;//6 1
-			pin_data[1] = (forward - clockwise) + 100;//7 2
-
-			pin_data[2] = (up) + 100;//2 4
-			pin_data[3] = (up) + 100;//4 5
-		}
 		try
 		{
 			if (portIndex != -1)
@@ -349,7 +219,6 @@ int main (int, char**)
 
 	glfwDestroyWindow(window);
     glfwTerminate();
-
 
 	return 0;
 }
